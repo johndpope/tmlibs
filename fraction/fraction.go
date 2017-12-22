987c9a2efb017996b8769d877ff151927ca45dfe
rigelrozanski <rigel.rozanski@gmail.com>
2017-12-22 07:43:07 +0000

    fraction testing working

--- fraction/fraction.go
+++ fraction/fraction.go
@@ -1,89 +1,86 @@
-package Fraction
+package fraction
 
 import wire "github.com/tendermint/go-wire"
 
 // XXX test fractions!
 
-// FractionI -  basic fraction functionality
-// TODO better name that FractionI?
-type FractionI interface {
-	Inv() FractionI
-	SetNumerator(int64) FractionI
-	SetDenominator(int64) FractionI
+// Fraction -  basic fraction functionality
+// TODO better name that Fraction?
+type Fraction interface {
 	GetNumerator() int64
 	GetDenominator() int64
-	Simplify() FractionI
+	RectifySign() Fraction
+	Inv() Fraction
+	Simplify() Fraction
 	Negative() bool
 	Positive() bool
-	GT(FractionI) bool
-	LT(FractionI) bool
-	Equal(FractionI) bool
-	Mul(FractionI) FractionI
-	Div(FractionI) FractionI
-	Add(FractionI) FractionI
-	Sub(FractionI) FractionI
+	GT(Fraction) bool
+	LT(Fraction) bool
+	Equal(Fraction) bool
+	Mul(Fraction) Fraction
+	Div(Fraction) Fraction
+	Add(Fraction) Fraction
+	Sub(Fraction) Fraction
 	Evaluate() int64
 }
 
-// Fraction - basic fraction
-type Fraction struct {
+// fraction - basic fraction
+type fraction struct {
 	Numerator, Denominator int64
 }
 
-var _ FractionI = Fraction{} // enforce at compile time
-var _ = wire.RegisterInterface(struct{ FractionI }{}, wire.ConcreteType{Fraction{}, 0x01})
+var _ Fraction = fraction{} // enforce at compile time
+var _ = wire.RegisterInterface(struct{ Fraction }{}, wire.ConcreteType{fraction{}, 0x01})
 
-// NewFraction - create a new fraction object
+// New - create a new fraction object
 func New(Numerator int64, Denominator ...int64) Fraction {
 	switch len(Denominator) {
 	case 0:
-		return Fraction{Numerator, 1}
+		return fraction{Numerator, 1}
 	case 1:
-		return Fraction{Numerator, Denominator[0]}
+		return fraction{Numerator, Denominator[0]}
 	default:
 		panic("improper use of NewFraction, can only have one denominator")
 	}
 }
 
-// SetNumerator - return a fraction with a new Numerator
-func (f Fraction) SetNumerator(Numerator int64) FractionI {
-	return Fraction{Numerator, f.Denominator}
-}
-
-// SetDenominator - return a fraction with a new Denominator
-func (f Fraction) SetDenominator(Denominator int64) FractionI {
-	return Fraction{f.Numerator, Denominator}
-}
-
 // GetNumerator - return the Numerator
-func (f Fraction) GetNumerator() int64 {
+func (f fraction) GetNumerator() int64 {
 	return f.Numerator
 }
 
 // GetDenominator - return the Denominator
-func (f Fraction) GetDenominator() int64 {
+func (f fraction) GetDenominator() int64 {
 	return f.Denominator
 }
 
+// RectifySign - make any negative sign exist only in Numerator
+func (f fraction) RectifySign() Fraction {
+	if f.Denominator < 0 {
+		f.Denominator *= -1
+		f.Numerator *= -1
+	}
+	return f
+}
+
 // Inv - Inverse
-func (f Fraction) Inv() FractionI {
-	return Fraction{f.Denominator, f.Numerator}
+func (f fraction) Inv() Fraction {
+	return fraction{f.Denominator, f.Numerator}
 }
 
 // Simplify - find the greatest common Denominator, divide
-func (f Fraction) Simplify() FractionI {
-
+func (f fraction) Simplify() Fraction {
 	gcd := f.Numerator
-
 	for d := f.Denominator; d != 0; {
 		gcd, d = d, gcd%d
 	}
-
-	return Fraction{f.Numerator / gcd, f.Denominator / gcd}
+	f.Numerator /= gcd
+	f.Denominator /= gcd
+	return f.RectifySign()
 }
 
 // Negative - is the fractior negative
-func (f Fraction) Negative() bool {
+func (f fraction) Negative() bool {
 	switch {
 	case f.Numerator > 0:
 		if f.Denominator > 0 {
@@ -100,7 +97,7 @@ func (f Fraction) Negative() bool {
 }
 
 // Positive - is the fraction positive
-func (f Fraction) Positive() bool {
+func (f fraction) Positive() bool {
 	switch {
 	case f.Numerator > 0:
 		if f.Denominator > 0 {
@@ -117,69 +114,75 @@ func (f Fraction) Positive() bool {
 }
 
 // Equal - test if two Fractions are equal, does not simplify
-func (f Fraction) Equal(f2 FractionI) bool {
+func (f fraction) Equal(f2 Fraction) bool {
 	if f.Numerator == 0 {
 		return f2.GetNumerator() == 0
 	}
-	return ((f.Numerator == f2.GetNumerator()) && (f.Denominator == f2.GetDenominator()))
+	f1 := f.RectifySign()
+	f2 = f2.RectifySign()
+	return ((f1.GetNumerator() == f2.GetNumerator()) &&
+		(f1.GetDenominator() == f2.GetDenominator()))
 }
 
 // GT - greater than
-func (f Fraction) GT(f2 FractionI) bool {
+func (f fraction) GT(f2 Fraction) bool {
 	return f.Sub(f2).Positive()
 }
 
 // LT - less than
-func (f Fraction) LT(f2 FractionI) bool {
+func (f fraction) LT(f2 Fraction) bool {
 	return f.Sub(f2).Negative()
 }
 
 // Mul - multiply
-func (f Fraction) Mul(f2 FractionI) FractionI {
-	return Fraction{
+func (f fraction) Mul(f2 Fraction) Fraction {
+	return fraction{
 		f.Numerator * f2.GetNumerator(),
 		f.Denominator * f2.GetDenominator(),
 	}.Simplify()
 }
 
 // Div - divide
-func (f Fraction) Div(f2 FractionI) FractionI {
-	return Fraction{
+func (f fraction) Div(f2 Fraction) Fraction {
+	if f2.GetNumerator() == 0 {
+		panic("fraction divide by zero error!")
+	}
+	return fraction{
 		f.Numerator * f2.GetDenominator(),
 		f.Denominator * f2.GetNumerator(),
 	}.Simplify()
 }
 
 // Add - add without simplication
-func (f Fraction) Add(f2 FractionI) FractionI {
+func (f fraction) Add(f2 Fraction) Fraction {
 	if f.Denominator == f2.GetDenominator() {
-		return Fraction{
+		return fraction{
 			f.Numerator + f2.GetNumerator(),
 			f.Denominator,
 		}.Simplify()
 	}
-	return Fraction{
+	return fraction{
 		f.Numerator*f2.GetDenominator() + f2.GetNumerator()*f.Denominator,
 		f.Denominator * f2.GetDenominator(),
 	}.Simplify()
 }
 
 // Sub - subtract without simplication
-func (f Fraction) Sub(f2 FractionI) FractionI {
+func (f fraction) Sub(f2 Fraction) Fraction {
 	if f.Denominator == f2.GetDenominator() {
-		return Fraction{
+		return fraction{
 			f.Numerator - f2.GetNumerator(),
 			f.Denominator,
 		}.Simplify()
 	}
-	return Fraction{
+	return fraction{
 		f.Numerator*f2.GetDenominator() - f2.GetNumerator()*f.Denominator,
 		f.Denominator * f2.GetDenominator(),
 	}.Simplify()
 }
 
 // Evaluate - evaluate the fraction using bankers rounding
-func (f Fraction) Evaluate() int64 {
+func (f fraction) Evaluate() int64 {
 
 	d := f.Numerator / f.Denominator // always drops the decimal
 	if f.Numerator%f.Denominator == 0 {
